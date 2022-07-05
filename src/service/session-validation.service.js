8604304055b1f0ae10aeb0a29379a8ca98414394
Aluisio <aluisioalves123@gmail.com>
2022-07-05 10:40:33 -0300

    403 when acessing via another scope

--- src/service/session-validation.service.js
+++ src/service/session-validation.service.js
@@ -5,17 +5,22 @@ const __dirname = path.resolve();
 export function isCustomer(request, response, next) {
   if (request.session.isCustomer)
     return next();
-
+  else
+    response.sendFile(path.join(__dirname+ '/public/403.html'));
 }
 
 export function isMerchant(request, response, next) {
   if (request.session.isMerchant)
     return next();
+  else
+    response.sendFile(path.join(__dirname+ '/public/403.html'));
 }
 
 export function isAdmin(request, response, next) {
   if (request.session.isAdmin)
     return next();
+  else
+    response.sendFile(path.join(__dirname+ '/public/403.html'));
 }
 
 export function isSignedIn(request, response, next) {

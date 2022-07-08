5ec64e4de82689e036de0c52c43e98859f252c5d
Gustavo <lgx.0097@gmail.com>
2022-07-08 17:23:08 -0300

    mudando logica do data access para usar o prisma

--- src/model/data-access.model.js
+++ src/model/data-access.model.js
@@ -1,97 +1,51 @@
 import axios from  'axios'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default class DataAccessModel {
   static async list() {
+    const registers = await prisma[this.tableName].findMany({})
+
     let list = []
-    const params = new URLSearchParams({ "table": this.tableName})
-    await axios
-      .get('http://localhost:3001/list', { params })
-      .then(res => {
-        res.data.forEach((register) => {
-          list.push(new this(register))
-        })
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    registers.forEach((register) => {
+      list.push(new this(register))
+    })
 
     return list
   }
 
   static async find(id) {
-    let register
-    const params = new URLSearchParams({ "table": this.tableName, "id": id })
-    await axios
-      .get('http://localhost:3001/find', { params })
-      .then(res => {
-        register = new this(res.data) 
-      })
-      .catch(error => {
-        console.error(error);
-      });
-
-    return register
+    const register = await prisma[this.tableName].findUnique({ where: { id: parseInt(id) }})
+    return new this(register) 
   }
 
   static async findByField(field, value) {
-    let register
-    const params = new URLSearchParams({ "table": this.tableName, "field": field, "value": value})
-    await axios
-      .get('http://localhost:3001/findByField', { params })
-      .then(res => {
-        register = new this(res.data) 
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    const register = await prisma[this.tableName].findUnique({ where: { [field]:value } })
 
-    return register
+    return new this(register)
   }
 
   static async delete(id) {
-    const params = new URLSearchParams({ "table": this.tableName, "id": id })
-    await axios
-      .delete('http://localhost:3001/delete', { params })
-      .then(res => {
-        res.data
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    await prisma[this.tableName].delete({ where: { id: id }})
   }
 
   static async create(params) {
-    // validando campos
     let paramsToCreate = {}
     this.fields.forEach((field) => {
-      if (params[field] !== null && params[field] !== undefined) {
+      if (params[field] !== null) {
         paramsToCreate[field] = params[field]
+      } else {
+        paramsToCreate[field] = null
       }
     })
 
-    //construido payload
-    const payload = JSON.stringify({ 
-      "table": this.tableName,
-      "params": paramsToCreate 
-    })
+    const register = await prisma[this.tableName].create({ data: paramsToCreate })
 
-    let register
-    const res = await axios.post('http://localhost:3001/create', payload, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(res => {
-      register = new this(res.data) 
-    })
-    .catch(error => {
-      console.error(error)
-    })
-
-    return register
+    return new this(register)
   }
 
   static async update(id, params) {
-    // validando campos
     let paramsToUpdate = {}
     this.fields.forEach((field) => {
       if (params[field] !== null && params[field] !== undefined) {
@@ -99,25 +53,11 @@ export default class DataAccessModel {
       }
     })
 
-    //construido payload
-    const payload = JSON.stringify({ 
-      "table": this.tableName,
-      "id": id,
-      "params": paramsToUpdate 
+    const register = await prisma[this.tableName].update({
+      where: { id: id },
+      data: paramsToUpdate,
     })
 
-    let register
-    const res = await axios.put('http://localhost:3001/update', payload, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(res => {
-      register = new this(res.data) 
-    })
-    .catch(error => {
-      console.error(error)
-    })
-
-    return register
+    return new this(register)
   }
 }

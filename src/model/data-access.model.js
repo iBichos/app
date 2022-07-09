a44a60047be5896ed353eff1c9c05e114349ab14
Gustavo <lgx.0097@gmail.com>
2022-07-09 17:55:04 -0300

    add try catch para funções de acesso ao banco

--- src/model/data-access.model.js
+++ src/model/data-access.model.js
@@ -1,63 +1,85 @@
-import axios from  'axios'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 export default class DataAccessModel {
   static async list() {
-    const registers = await prisma[this.tableName].findMany({})
+    try {
+      const registers = await prisma[this.tableName].findMany({})
 
-    let list = []
-    registers.forEach((register) => {
-      list.push(new this(register))
-    })
+      let list = []
+      registers.forEach((register) => {
+        list.push(new this(register))
+      })
 
-    return list
+      return list
+    } catch(e) { 
+      console.log(e) 
+    }
   }
 
   static async find(id) {
-    const register = await prisma[this.tableName].findUnique({ where: { id: parseInt(id) }})
-    return new this(register) 
+    try {
+      const register = await prisma[this.tableName].findUnique({ where: { id: parseInt(id) }})
+      return new this(register)
+    } catch(e) { 
+      console.log(e) 
+    }
   }
 
   static async findByField(field, value) {
-    const register = await prisma[this.tableName].findUnique({ where: { [field]:value } })
-
-    return new this(register)
+    try {
+      const register = await prisma[this.tableName].findUnique({ where: { [field]:value } })
+      return new this(register)
+    } catch(e) { 
+      console.log(e) 
+    }
   }
 
   static async delete(id) {
-    await prisma[this.tableName].delete({ where: { id: parseInt(id) }})
+    try {
+      await prisma[this.tableName].delete({ where: { id: parseInt(id) }})
+    } catch(e) { 
+      console.log(e) 
+    }
   }
 
   static async create(params) {
-    let paramsToCreate = {}
-    this.fields.forEach((field) => {
-      if (params[field] !== null) {
-        paramsToCreate[field] = params[field]
-      } else {
-        paramsToCreate[field] = null
-      }
-    })
+    try {
+      let paramsToCreate = {}
+      this.fields.forEach((field) => {
+        if (params[field] !== null) {
+          paramsToCreate[field] = params[field]
+        } else {
+          paramsToCreate[field] = null
+        }
+      })
 
-    const register = await prisma[this.tableName].create({ data: paramsToCreate })
+      const register = await prisma[this.tableName].create({ data: paramsToCreate })
 
-    return new this(register)
+      return new this(register)
+    } catch(e) { 
+      console.log(e) 
+    }
   }
 
   static async update(id, params) {
-    let paramsToUpdate = {}
-    this.fields.forEach((field) => {
-      if (params[field] !== null && params[field] !== undefined) {
-        paramsToUpdate[field] = params[field]
-      }
-    })
+    try {
+      let paramsToUpdate = {}
+      this.fields.forEach((field) => {
+        if (params[field] !== null && params[field] !== undefined) {
+          paramsToUpdate[field] = params[field]
+        }
+      })
 
-    const register = await prisma[this.tableName].update({
-      where: { id: id },
-      data: paramsToUpdate,
-    })
+      const register = await prisma[this.tableName].update({
+        where: { id: id },
+        data: paramsToUpdate,
+      })
 
-    return new this(register)
+      return new this(register)
+    } catch(e) { 
+      console.log(e) 
+    }
   }
 }
